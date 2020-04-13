5d96059fda287a18f9fb5341687492a00680f145
walshl2 <walshl2@BY1928A75001814.wit.private>
2020-04-13 14:51:26 -0400

    Added space for more notes, wrapped gridPane in scrollPane

--- FinalProject/src/application/SampleController.java
+++ FinalProject/src/application/SampleController.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
 public class SampleController extends Main {
@@ -13,14 +22,43 @@ public class SampleController extends Main {
 
     @FXML
     private Button newNoteBtn;
+    
+    @FXML
+    public GridPane gridPane;
 
+    //creates a note and adds it to the array of notes
     @FXML
     void makeNote(ActionEvent event) {
     	if (!noteMessage.getText().isBlank()) {
-    		super.notes.add(new Note(20, Color.LIGHTPINK, noteMessage.getText()));
-    		System.out.println(noteMessage.getText());	
+    		if (notes.size() % 3 == 0) {
+    			gridPane.resize(screenWidth, gridPane.getHeight() + 50);
+    			gridPane.addRow(gridPane.getRowCount());
+    		}
+    		Note note = new Note(20, Color.LIGHTPINK, noteMessage.getText());
+    		super.notes.add(note);
+    		System.out.println(noteMessage.getText());
+    		gridPane.add(note.createTextArea(note), (notes.size() - 1) % 3, (notes.size() - 1) / 3);
     	}
     	noteMessage.clear();
 	}
+    
+    //deletes a note when it's selected by the user and the user hits delete
+    //the notes will then reorganize themselves to get rid of the gap
+    @FXML
+    void deleteNote(KeyEvent event) {
+    	if (event.getCode() == KeyCode.DELETE) {
+    		System.out.println("--Note Deleted--");
+    		Object target = event.getTarget();
+    		notes.remove(gridPane.getChildren().indexOf(target));
+    		if (target instanceof Node) {
+    			gridPane.getChildren().remove(target);
+    			gridPane.getChildren().clear();
+    			
+    			for (int i = 0; i < notes.size(); i++) {
+    				gridPane.add(notes.get(i).createTextArea(notes.get(i)), i % 3, i / 3);
+    			}
+    		}
+    	}
+    }
 
 }
