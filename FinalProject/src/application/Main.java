41adf9abb56a1b660d7bde708e6c289681347717
walshl2 <walshl2@BY1928A75001814.wit.private>
2020-04-13 14:31:14 -0400

    Notes can be created/deleted now

--- FinalProject/src/application/Main.java
+++ FinalProject/src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javafx.application.Application;
@@ -11,25 +12,25 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	
-	static public ArrayList<Note> notes = new ArrayList<Note>();
 	static public int screenWidth = 650;
-	static public int screenHeight = 525;
+	static public int screenHeight = 635;
 	static public Scene scene = null;
+	static public ArrayList<Note> notes = new ArrayList<Note>();
+	static public VBox root;
 	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			scene = new Scene(root,screenWidth,screenHeight);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws IOException{
+		root = (VBox)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+		scene = new Scene(root,screenWidth,screenHeight);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Notes");
+		primaryStage.show();
+		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+
 	}
 }
--- FinalProject/src/application/Note.java
+++ FinalProject/src/application/Note.java
@@ -6,28 +6,27 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.fxml.FXMLLoader;
 
 public class Note extends Main {
 	
 	private int size;
 	private Color color;
-	private String message = "Help";
-	private TextField field;
+	private String message = "";
 	
 	protected Note() {}
 	
 	protected Note(int s, Color c, String m) {
 		size = s;
 		color = c;
-		m = message;
-		
-		field = new TextField();
-		
-		
+		message = m;
 	}
 	
 	public void setColor(Color c) {
@@ -48,6 +47,21 @@ public class Note extends Main {
 	
 	public void setMessage(String m) {
 		message = m;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	//creates the note text area
+	public TextArea createTextArea(Note n) {
+		TextArea area = new TextArea(n.getMessage());
+		area.setWrapText(true);
+		area.setStyle("-fx-control-inner-background:white");
+		area.setFont(Font.font(25));
+		area.resize(size, size);
+		area.setEditable(false);
+		return area;
 	}
 
 }
